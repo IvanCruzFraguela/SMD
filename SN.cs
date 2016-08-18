a09c7d074cee226d5bc0707ad80d132cbc658c4b
ivancruz <ivancruzfraguela@gmail.com>
2016-08-18 12:46:11 +0200

    Initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .vs/SMD/v14/.suo
    	new file:   Properties/AssemblyInfo.cs
    	new file:   SMD.csproj
    	new file:   SMD.sln
    	new file:   SN.cs

---
+++ SN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMD
+{
+    public class SN {
+        public const string id = "Id";
+        public const string nomb = "Nomb";
+        public const string descr = "Descr";
+        public const string idPadre = "IdPadre";
+        public const string mom = "Mom";
+    }
+}
